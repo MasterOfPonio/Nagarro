6d87cd2a258c858d16699758339894a6ff149399
gcalancea <gabriel.calancea@verint.com>
2019-10-31 17:56:06 +0200

    Work on controllers

--- force-app/main/default/aura/CreditCheckLC/CreditCheckLCController.js
+++ force-app/main/default/aura/CreditCheckLC/CreditCheckLCController.js
@@ -1,10 +1,59 @@
 ({
 	doInit : function(component, event, helper) {
-		var action = cmp.get("c.CreateNewCase");
+		
+	},
+
+	handleSave: function(component,event,helper) {
+		var action = component.get("c.openCaseExist");
+		action.setParams({
+			accID: component.get("v.recordId")
+			
+		});
+	
+		var inputsel = component.find("v.CreditCheck");
+		console.log("inputsel: "+inputsel);
+		action.setCallback(this, function(response) {
+			var state = response.getState();
+            
+            if (state === "SUCCESS") {
+				var caseExists = response.getReturnValue();
+				console.log("caseExists: "+caseExists);
+				if (caseExists == true) {
+					var resultsToast = $A.get("e.force:showToast");
+				
+        		resultsToast.setParams({
+            			"title": "Open case Already exists!",
+            			"message": "An open case has already been submitted and its pending resolutions."
+				});
+				
+				resultsToast.fire();
+				$A.get("e.force:closeQuickAction").fire();
+				
+				} 
+            }
         
-        action.setCallBack(this, function(response) {
-        	var success = response.getReturnValue();
-        });
-        $A.enqueueAction(action);
+            else if (state === "INCOMPLETE") {
+              
+            }
+     
+            else if (state === "ERROR") {
+                var errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log("Error message: " + 
+                                 errors[0].message);
+                    }
+                } else {
+                    console.log("Unknown error");
+                }
+            }
+
+
+
+			
+			
+		});
+		$A.enqueueAction(action); 
 	}
+
 })
