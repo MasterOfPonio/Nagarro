a36aba5e12dda605cad8862157889c601cf2b42d
Gabi <gabi.calancea@gmail.com>
2019-11-03 20:13:02 +0200

    fixed last bugs

--- force-app/main/default/aura/CreditCheckLC/CreditCheckLCController.js
+++ force-app/main/default/aura/CreditCheckLC/CreditCheckLCController.js
@@ -1,35 +1,47 @@
 ({
 	doInit : function(component, event, helper) {
 		
+	
 	},
 
 	handleSave: function(component,event,helper) {
+
 		var action = component.get("c.openCaseExist");
+
+		var recId = component.get("v.recordId");
+		
+		var creditOptionSelected = component.find("CreditCheck").get("v.value");
 		action.setParams({
-			accID: component.get("v.recordId")
-			
+			accID: recId,
+			CreditCheck: creditOptionSelected
 		});
-	
-		var inputsel = component.find("v.CreditCheck");
-		console.log("inputsel: "+inputsel);
+		
 		action.setCallback(this, function(response) {
 			var state = response.getState();
             
             if (state === "SUCCESS") {
 				var caseExists = response.getReturnValue();
-				console.log("caseExists: "+caseExists);
 				if (caseExists == true) {
 					var resultsToast = $A.get("e.force:showToast");
 				
-        		resultsToast.setParams({
-            			"title": "Open case Already exists!",
-            			"message": "An open case has already been submitted and its pending resolutions."
-				});
+					resultsToast.setParams({
+							"title": "Open case Already exists!",
+							"message": "An open case has already been submitted and its pending resolutions."
+					});
+					
+					resultsToast.fire();
+					$A.get("e.force:closeQuickAction").fire();
+				} else {
+					var resultsToast = $A.get("e.force:showToast");
 				
-				resultsToast.fire();
-				$A.get("e.force:closeQuickAction").fire();
-				
-				} 
+					resultsToast.setParams({
+							"title": "Case was created on current account.",
+							"message": "Case was added to the account" 
+					});
+					
+					resultsToast.fire();
+					$A.get("e.force:closeQuickAction").fire();
+				}
             }
         
             else if (state === "INCOMPLETE") {
@@ -46,14 +58,12 @@
                 } else {
                     console.log("Unknown error");
                 }
-            }
-
-
-
-			
+            }			
 			
 		});
 		$A.enqueueAction(action); 
+		
+		
 	}
 
 })
